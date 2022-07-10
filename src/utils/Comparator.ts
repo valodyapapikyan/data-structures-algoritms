ef493a523d661b2ee0b0bbf466914a532b907ff6
Valodya Papikyan <valodyapapikyan@Valodyas-MacBook-Pro.local>
2022-07-10 22:04:29 +0400

    feat/linkedList implementation

---
+++ src/utils/Comparator.ts
@@ -0,0 +1,22 @@
+export class Comparator<T> {
+
+    compare: (a: T, b: T) => 0 | 1 | -1;
+
+    constructor() {
+        this.compare = this.defaultComparator;
+    };
+
+    defaultComparator(a: T, b: T) {
+        if (a === b) {
+            return 0;
+        }
+
+        return a < b ? -1 : 1;
+    }
+
+    isEqual(a: T, b: T) {
+        return this.compare(a, b)
+    }
+
+    // todo to be implemented lessThan, greaterThan
+}
