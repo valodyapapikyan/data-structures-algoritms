5a88aeb21f9eacff4d98c7ec2a1ca48060208ab7
Valodya Papikyan <valodyapapikyan@Valodyas-MacBook-Pro.local>
2022-07-17 18:31:51 +0400

    feat/ doubly linked list interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,12 +1,27 @@
 import { Node } from './../data-structures/LinkedList/node';
 
-export interface ILinkedList<T> {
+
+interface IGeneral<T> {
+  size(): number;
+  traverse(): T[];
+  isEmpty(): boolean,
+  search(data: T): Node<T> | null;
+}
+
+export interface ILinkedList<T> extends IGeneral<T> {
   insertFirst(data: T): Node<T>,
   insertLast(data: T): Node<T>,
   deleteFirst(): boolean,
   deleteLast(): boolean,
-  traverse(): T[];
-  size(): number;
-  search(data: T): Node<T> | null;
+
 }
 
+
+export interface IDoublyLinkedList<T> extends IGeneral<T> {
+  first(): T,
+  last(): T,
+  addFirst(element: T): void,
+  addLast(element: T): void,
+  removeFisrt(): T,
+  removeLast(): T
+}
